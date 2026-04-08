2322790ed370f52ea76426213e300aac96b58388
isabellysilvaxavier-max <isabelly.silva.xavier@escola.pr.gov.br>
2026-04-08 11:06:49 -0300

    Add files via upload

---
+++ testa.js
@@ -0,0 +1,22 @@
+//1.exiba a mensage "ola mundo" ao console.
+console.log("ola mundo");
+//2.crie uma variável com seu nome e exiba o valor.
+let nome= "isabelly";
+console.log(nome);
+//3.soma dois numeros e exiba o resultado.
+let soma = 2 + 2;
+console.log(soma);
+//4.subtraia dois numeros e exiba o resultado.
+let subtraia = 8 - 7;
+console.log(subtracao);
+//5. mutiplique dois numeros e exiba o resultado.
+let mutiplicacao = 7 * 6;
+console.log(mutiplicacao);
+//6.descubra o resto da divisao de um numero por outro.
+let resto = 10 % 3;
+console.log(resto);
+//7.crie uma variavel boleana ( tru e false) e exiba seu valor.
+let estaChovendo = true;
+console.log(estaChovendo);
+//8.verifique se o numero e maior que o outro 
+let 
